82fc9a94fe776d5b544cf6c35d5fa37cab1b7640
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2019-07-23 15:17:49 +0100

    initial commit

---
+++ elpa/scad-mode-20190413.1246/scad-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "scad-mode" "20190413.1246" "A major mode for editing OpenSCAD code" 'nil :commit "6cc7e911f606a7c59a0501299ec559376dae9583" :keywords '("languages") :authors '(("Len Trigg, Łukasz Stelmach")) :maintainer '("Len Trigg" . "lenbok@gmail.com") :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el")
---
+++ init.el
@@ -0,0 +1,20 @@
+(require `package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-check-signature nil)
+(package-initialize)
+(autoload 'scad-mode "scad-mode" "A major mode for editing OpenSCAD code." t)
+(add-to-list 'auto-mode-alist '("\\.scad$" . scad-mode))
+(setq tab-width 2)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (scad-preview scad-mode)))
+ '(scad-command "\"c:\\Program Files\\OpenSCAD\\openscad.exe\""))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
