3c3aa5d470661944d7cb9364b517e6e36eacf47c
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2019-07-24 00:55:02 +0100

    * better tab handling in scad-mode * better settings for the scad-preview-mode

--- init.el
+++ init.el
@@ -1,17 +1,26 @@
+(setq-default
+ c-default-style "bsd"
+ c-basic-offset 2
+)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode t)
+(normal-erase-is-backspace-mode 1)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "C-?") 'help-command)
+
 (require `package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (setq package-check-signature nil)
 (package-initialize)
-(autoload 'scad-mode "scad-mode" "A major mode for editing OpenSCAD code." t)
-(add-to-list 'auto-mode-alist '("\\.scad$" . scad-mode))
-(setq tab-width 2)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (scad-preview scad-mode)))
- '(scad-command "\"c:\\Program Files\\OpenSCAD\\openscad.exe\""))
+ '(scad-command "c:\\Tools\\OpenSCAD\\openscad.exe")
+ '(scad-preview-image-size (quote (1600 . 1800))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
