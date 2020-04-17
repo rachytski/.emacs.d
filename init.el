b870d8b251d9481f24d71d9431050f6b8fb4f81a
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-04-18 00:14:40 +0100

    allow standard copy-cut-insert shortcuts to work in emacs

--- init.el
+++ init.el
@@ -74,6 +74,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
