105934e281558ecf02e8e7f92a2445db186ee6ad
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-04-24 19:04:28 +0100

    keeping custom-set-variables and custom-set-faces in a separate file.

--- init.el
+++ init.el
@@ -1,10 +1,3 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (defconst user-init-dir
   (cond ((boundp 'user-emacs-directory)user-emacs-directory)
         ((boundp 'user-init-directory)user-init-directory)
@@ -15,25 +8,11 @@
   "Load a file in current user's configuration directory"
   (load-file (expand-file-name file user-init-dir)))
 
-
+(setq custom-file (expand-file-name "custom.el" user-init-dir))
+      
 (load-user-file "platform.el")
 (load-user-file "packages.el")
 (load-user-file "general.el")
 (load-user-file "openscad.el")
 (load-user-file "c++.el")
 (load-user-file "python.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (jedi zygospore use-package solarized-theme scad-preview company-jedi company-irony))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
