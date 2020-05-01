1c0c9fd8041801a9fb2ab3f5f27e6515c4e53053
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-01 22:10:13 +0000

    added descriptive comment on using 256-color terminal palette

--- colors.el
+++ colors.el
@@ -1,1 +1,5 @@
+;; if colors are off, make sure you run your terminal
+;; in more than 16-colors palette. the following
+;; should help on *NIX systems 
+;; export TERM=xterm-256color
 (load-theme `solarized-light t t)
