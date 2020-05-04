7ae2670e95b6231876798da0d889575143ada5ba
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 21:16:00 +0100

    cleaned up the code for checking and auto-installing packages as currently it's done through use-package's :ensure variable

--- packages.el
+++ packages.el
@@ -5,24 +5,3 @@
 (setq package-check-signature nil)
 (setq package-user-dir elpa-personal-path)
 (package-initialize)
-
-(defvar my-selected-packages '(solarized-theme
-                               use-package                               
-                               jedi
-                               company-jedi
-                               scad-preview
-                               scad-mode)
-  "default packages")
-
-(defun my-packages-installed-p ()
-  (loop for pkg in my-selected-packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-
-(unless (my-packages-installed-p)
-  (message "%s" "refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg my-selected-packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
