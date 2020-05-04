790ccbe3496da22c95d5b104f4fa0f0d49763bb5
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 21:13:49 +0100

    cleaner separation of packages configuration for ide-like features

---
+++ company.el
@@ -0,0 +1,15 @@
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  :bind (("C-/" . company-complete-common-or-cycle)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous)
+         ("C-h" . delete-backward-char))
+  :config
+  (unbind-key "M-n" company-active-map)
+  (unbind-key "M-p" company-active-map)
+  (delete `company-clang company-backends)
+  )
---
+++ lsp.el
@@ -0,0 +1,19 @@
+
+
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+
