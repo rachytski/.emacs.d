ccff5faab90fd75c78b5757832307a91094546c7
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 21:44:25 +0100

    settings up beautiful FantasqueSansMono font as a default

--- colors.el
+++ colors.el
@@ -3,3 +3,7 @@
 ;; should help on *NIX systems 
 ;; export TERM=xterm-256color
 (load-theme `solarized-light t t)
+
+;; https://github.com/belluzj/fantasque-sans
+;; v1.8.0 is in fonts/ subfolder of this repo
+(set-frame-font "Fantasque Sans Mono-16" nil t)
