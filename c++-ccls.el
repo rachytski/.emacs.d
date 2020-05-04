e59d49e84aa3ec3c581f33a7b5bdb4a36152a934
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 22:50:56 +0100

    fixed error in path to ccls

--- c++-ccls.el
+++ c++-ccls.el
@@ -1,6 +1,6 @@
 (use-package ccls
   :ensure t
-  :init(setq ccls-executable (expand-file-name "builds/ccls" elpa-personal-path))
+  :init(setq ccls-executable (expand-file-name "builds/ccls/ccls" elpa-personal-path))
   :hook((c-mode c++-mode) . (lambda () (require `ccls) (lsp))))
 
 
