793421604d1f083c09e0df06d9004108bd2ab18f
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 22:54:08 +0100

    cleanup and comments

--- colors.el
+++ colors.el
@@ -7,3 +7,10 @@
 ;; https://github.com/belluzj/fantasque-sans
 ;; v1.8.0 is in fonts/ subfolder of this repo
 (set-frame-font "Fantasque Sans Mono-16" nil t)
+
+;; Some other options taken from
+;;http://pragmaticemacs.com/emacs/fun-with-fonts/
+;(set-frame-font "DejaVu Sans Mono-14" nil t)
+;(set-frame-font "Source Code Pro-14" nil t)
+;(set-frame-font "Monaco-14" nil t)
+;(set-frame-font "Cousine-14" nil t)
--- general.el
+++ general.el
@@ -1,22 +1,29 @@
+;; Allows emacs to inherit parent shell environment,
+;; instead of using system-wide ones
 (use-package exec-path-from-shell
   :ensure t)
 
+;; Allows mission-control-like switching between emacs buffers
+;; Initiate with Ctrl-Tab, continue switching with Tab.
+;; Note, that Ctrl-I is the same as Tab, so switching is
+;; eeringly similar to Visual Studio tab switching behaviour
+;; (which is the goal btw, as its super convenient)
 (use-package buffer-expose
   :ensure t
   :bind(("<C-tab>" . buffer-expose-no-stars))
-)
+  )
 
 (unless (file-exists-p "~/.emacs.d/.backups/") (make-directory "~/.emacs.d/.backups/") )
 (unless (file-exists-p "~/.emacs.d/.autosaves/") (make-directory "~/.emacs.d/.autosaves/") )
 
-; storing all backups in a separate dir
+; Storing all backups in a separate dir
 (setq backup-by-copying t ; copy, don't symlink
       backup-directory-alist `(("." . "~/.emacs.d/.backups/"))
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
       version-control t)
-; storing all autosaves in a separate dir
+; Storing all autosaves in a separate dir
 (setq auto-save-file-name-transforms
       `((".*" "~/.emacs.d/.autosaves/" t)))
 
@@ -25,19 +32,21 @@
 ; indenting with spaces
 (setq-default indent-tabs-mode nil)
 (normal-erase-is-backspace-mode 1)
-; C-h is faster than backspace
+; C-h adds less wrist strain than backspace.
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-?") 'help-command)
 
+;; No startup screen, go straight to the point
 (setq inhibit-startup-screen t)
 (cua-mode t)
 
+;; Switching off parts of interface for lean-and-clean look 
 ;(scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (show-paren-mode t)
 
-; zygospore package is a lifesaver!
+;; zygospore package is a lifesaver!
 (use-package zygospore
   :ensure t
   :bind(("C-x 1" . 'zygospore-toggle-delete-other-windows))
