3046f019e506558737cee0566ac5595e7b0e39a0
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-04 21:11:41 +0100

    cleanup custom.el, put empty package-selected-packages variable to block package manager from littering the list of installed packages

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zygospore use-package solarized-theme scad-preview jedi company-jedi company-irony))))
+ '(package-selected-packages '()))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
