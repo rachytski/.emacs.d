2989cac5771399d9f6b3bd44f6d65290cb6e92cc
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-11 00:33:18 +0100

    forgot to add projectile.el in on of prev commits

---
+++ projectile.el
@@ -0,0 +1,6 @@
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode +1))
