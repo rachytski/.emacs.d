12b17d429ee3b0880304ce7ed429c152e159f14d
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-11 20:08:28 +0100

    added C-, keybinding to switch between corresponding files in project

--- projectile.el
+++ projectile.el
@@ -3,4 +3,5 @@
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-,") 'projectile-find-other-file)
   (projectile-mode +1))
