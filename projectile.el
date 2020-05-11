5aad12636a0311eb6daef1f9747396f2da41ef6a
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-11 22:24:53 +0100

    using ivy completion mode for projectile

--- projectile.el
+++ projectile.el
@@ -1,7 +1,11 @@
+(use-package ivy
+  :ensure t)
+
 (use-package projectile
   :ensure t
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-,") 'projectile-find-other-file)
+  (setq projectile-completion-system 'ivy)
   (projectile-mode +1))
