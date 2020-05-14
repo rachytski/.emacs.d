96b27b92eaf4416c40631e4af9f8dea401c7a395
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-14 22:08:58 +0100

    cleaned up openscad developement mode

--- openscad.el
+++ openscad.el
@@ -1,5 +1,8 @@
 (use-package scad-mode
-  :ensure t)
+  :ensure t
+  :init
+  (setq scad-command scad-full-path)
+  (setq scad-preview-image-size (quote (1600 . 1800))))
 
-(setq scad-command scad-full-path)
-(setq scad-preview-image-size (quote (1600 . 1800))) 
+
+
