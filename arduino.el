2206ef055934621d49ad77dbcdf5cd943048158b
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-15 13:23:12 +0100

    cleaner setup of arduino mode with some extra comments

--- arduino.el
+++ arduino.el
@@ -1,13 +1,9 @@
-(use-package company-arduino
-  :ensure t
-  :config (add-to-list 'company-backends 'company-arduino))
-
+;; arduino-mode provides very basic c-mode derived
+;; syntax highlighting and few usefull functions to
+;; compile, verify and upload sketches.
+;; NB: no sophisticated lsp-mode code completion yet.
 (use-package arduino-mode
-  :ensure t
-  :mode "\\.ino$"
-  :config
-  (add-hook 'c++-mode-hook (lambda ()
-                             (lsp))))
+  :ensure t)
 
 
 
