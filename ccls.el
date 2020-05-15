074a218937f4cec70ea004e605f3cbc9e9744b1f
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-15 20:55:56 +0100

    fixed incorrect bracketing

--- ccls.el
+++ ccls.el
@@ -1,8 +1,8 @@
 (use-package ccls
   :ensure t
   :init(setq ccls-executable (expand-file-name "builds/ccls/ccls" elpa-personal-path))
-  :hook((c-mode c++-mode) . (lambda () (require `ccls) (lsp))))
+  :hook((c-mode c++-mode) . (lambda () (require `ccls) (lsp)))
   :config
-  `(ccls-initialization-options (quote(compilationDatabaseDirectory :build)))
+  `(ccls-initialization-options (quote(compilationDatabaseDirectory :build))))
 
 
