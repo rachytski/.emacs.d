5209c9f035462715d941cfc515ddbc8e3b34234f
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-16 14:31:57 +0100

    remove 'triple-ESC ruining windows layout' behaviour

--- keybindings.el
+++ keybindings.el
@@ -1,1 +1,7 @@
 (global-set-key (kbd "M-m") 'toggle-frame-maximized)
+
+; remove annoying behaviour of destroying windows layout on triple-ESC 
+(defadvice keyboard-escape-quit
+  (around keyboard-escape-quit-dont-close-windows activate)
+  (let ((buffer-quit-function (lambda () ())))
+    ad-do-it))
