2c8c351eefbe386ea03cf6e80ba8515639d7db0c
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-29 22:51:47 +0100

    added Golang devenv setup instructions

--- go.el
+++ go.el
@@ -1,10 +1,11 @@
-;; Install LSP-server for go first
-;; One option is to use gopls
-;; read installation instruction here
+;; Having proper support for Golang developement
+;; requires the following:
+;; 1. Download and install go, have it on PATH
+;; 2. Have proper GOPATH envvar setup - folder for local packages repo
+;; 3. Have proper GOROOT envvar setup - folder with Golang installation (root folder, not bin subfolder!)
+;; 4. Install gopls following instructions from here:
 ;; https://github.com/golang/tools/blob/master/gopls/doc/user.md
-;; NB: Make sure gopls is on the PATH for
-;; lsp-mode to pick it up automatically
-
+;; Make sure gopls.exe is also on PATH
 (use-package go-mode
   :ensure t
   :hook((go-mode . (lambda () (require 'go-mode) (lsp)))))
