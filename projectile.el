2552322b41e5b95f10ae0755219fb9f2d5661577
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-06-11 13:37:59 +0300

    using helm-projectile for better search and narrowing, initially for buffers and projects.

--- projectile.el
+++ projectile.el
@@ -4,8 +4,13 @@
 (use-package projectile
   :ensure t
   :bind (("C-," . 'projectile-find-other-file)
-         ("C-x p" . 'projectile-command-map)
-         ("<C-tab>" . 'projectile-find-file))
+         ("C-x p" . 'projectile-command-map))
+         ;("<C-tab>" . 'projectile-find-file))
   :config
     (setq projectile-completion-system 'ivy)
     (projectile-mode +1))
+
+(use-package helm-projectile
+  :ensure t
+  :bind (("<C-tab>" . 'helm-projectile-find-file)
+         ("C-x o" . 'helm-projectile-switch-project)))
