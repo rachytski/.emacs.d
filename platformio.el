37975d132c9a980ba91392a9703c359ca19eaa19
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2025-09-13 12:17:23 +0200

    PlatformIO projects improvements
    * Leveraging Projectile to add custom project type

--- platformio.el
+++ platformio.el
@@ -1,3 +1,19 @@
-(use-package platformio-mode
-  :ensure t
-  :hook((c-mode c++-mode) . platformio-conditionally-enable))
+;;;(use-package platformio-mode
+;;;  :ensure t
+;;;  :requires (projectile)
+;;;  :hook((c-mode c++-mode) . platformio-conditionally-enable)
+;;;  :config
+;;;  (setq async-debug nil)
+;;;  )
+
+(defun projectile-platformio-project-p (&optional dir)
+  "Check if a project contains platformio.ini file.
+When DIR is specified it checks DIR's project, otherwise it acts on the current project."
+  (projectile-verify-file "platformio.ini" dir))
+
+(projectile-register-project-type 'platformio #'projectile-platformio-project-p
+				  :project-file "platformio.ini"
+				  :compile "pio run"
+				  :run "pio run -t upload"
+				  :install "pio run -t upload"
+				  :test "pio run -t test")
