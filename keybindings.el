4149d28d712842467491da223b16f4dd00634018
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2025-09-13 12:07:17 +0200

    term-keys package integration for better use of emacs in terminals
    * To support wider variety of keybindings in emacs when using it from
    terminal we need to send custom escape sequences when certain keys are
    pressed in combination (e.g. C-.). xterm-keys package does just that.
    * Extra configuration is required for specific terminals to specify
    those escape sequences, see xterm-keys documentation on
    that.

--- keybindings.el
+++ keybindings.el
@@ -1,4 +1,19 @@
-(global-set-key (kbd "M-m") 'toggle-frame-maximized)
+;;; Using Emacs in terminal requires special way of sending some key combinations.
+;;; In particular, terminal emulators can't send correctly some combinations, like C-. C-,
+;;; Instead some terminals may be configured to send escape sequences, which will be
+
+;;; recognized as aforementioned key combinations by term-keys package and forwarded
+;;; for processing to Emacs. Not all terminals support such customization of sending
+;; custom escape sequences, refer to term-keys documentation to figure out which do.
+
+(use-package term-keys
+  :ensure t
+  :init (term-keys-mode t))
+
+(global-set-key (kbd "M-n") 'scroll-up)
+(global-set-key (kbd "M-p") 'scroll-down)
+
+;;(global-set-key (kbd "M-m") 'toggle-frame-maximized)
 
 ; remove annoying behaviour of destroying windows layout on triple-ESC 
 (defadvice keyboard-escape-quit
