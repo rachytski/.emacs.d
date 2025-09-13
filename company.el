98ee42435a6ce1fc3708862a60e6ef2cfd176b8a
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2025-09-13 12:15:24 +0200

    package configuration improvement
    * Using :custom section to customize package related variables
    * Usability improvements for code completion

--- company.el
+++ company.el
@@ -2,15 +2,19 @@
   :ensure t
   :init
   (global-company-mode)
-  (setq company-minimum-prefix-length 1
-        company-idle-delay 0.0)
-  :bind (("C-/" . company-complete-common-or-cycle)
-         :map company-active-map
-         ("C-n" . company-select-next)
-         ("C-p" . company-select-previous)
-         ("C-h" . delete-backward-char))
+  :bind
+  (;;;("C-/" . company-complete-common-or-cycle)
+   :map company-active-map
+   ("C-n" . company-select-next)
+   ("C-p" . company-select-previous)
+   ("C-h" . delete-backward-char))
   :config
   (unbind-key "M-n" company-active-map)
   (unbind-key "M-p" company-active-map)
   (delete `company-clang company-backends)
+  :custom
+  (company-minimum-prefix-length 2)
+  (company-idle-delay 0.0)
   )
+
+
--- ivy.el
+++ ivy.el
@@ -1,3 +1,5 @@
+; Ivy, Counsel and Swiper are parts os the same repo, closely working together
+; Ivy is also used as a completion system backend for projectile 
 (use-package ivy
   :ensure t
   :bind(:map ivy-minibuffer-map ("C-m" . 'ivy-alt-done)
@@ -5,24 +7,28 @@
                                 ("TAB" . 'ivy-partial))
   :config
   (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t
-        ivy-height 10
-        ivy-count-format ""
-        ivy-initial-inputs-alist nil
-        ivy-display-style 'fancy
-        ivy-re-builders-alist '((t . ivy--regex-plus))))
+  :custom 
+  (ivy-use-virtual-buffers t)
+  (ivy-height 10)
+  (ivy-count-format "")
+  (ivy-initial-inputs-alist nil)
+  (ivy-display-style 'fancy)
+  (ivy-re-builders-alist '((t . ivy--regex-plus))))
 
 (use-package counsel
   :ensure t
+  :config
+  (setq gud-key-prefix "\C-]") ; doesn't matter what, just to remove C-x C-a, which is bound to ag
   :bind(("C-x C-a" . 'counsel-rg) ;search in all files from current folder onwards
+	("C-x C-b" . 'counsel-projectile-ag) ;search only in project files
 	)
   )
 
-; enhances swiper for C-f command, also used for projectile
+
 (use-package swiper
   :ensure t
-  :bind("C-s" . 'swiper))
-
-
+  :bind(("C-s" . 'swiper)
+        )
+  )
 
 
