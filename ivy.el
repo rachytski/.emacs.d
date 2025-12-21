c48285fa0d781d2a7de1d6a204c26e6e0c149d68
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2025-12-21 00:09:39 +0000

    Better workflow for full-text search in projects
    * C-x C-a prefers codesearch when csearchindex is present in one of
    the parent folder, if not it uses rg
    * There's a way to call a specific command if required - C-x f hydra
    with c, r, a suffixes, which invoke codesearch, rg or ag respectively

--- ivy.el
+++ ivy.el
@@ -15,13 +15,39 @@
   (ivy-display-style 'fancy)
   (ivy-re-builders-alist '((t . ivy--regex-plus))))
 
+;; prioritizing codesearch over rg if .csearchindex is present in some parent directory
+(defun counsel-codesearch-or-rg ()
+  (interactive)
+  (if (codesearch--find-dominant-csearchindex default-directory)
+      (counsel-codesearch)
+    (counsel-rg)))
+
+;; as counsel-codesearch run async process to get results ivy--occur-default wouldn't be suitable.
+;; we should provide our own function which will produce occur results
+(defun counsel-codesearch-occur (&optional _cands)
+  "Generate a custom occur buffer for counsel-codesearch"
+  (counsel-grep-like-occur counsel-codesearch-command))
+
+(use-package counsel-codesearch
+  :ensure t
+  :init
+  (ivy-set-occur 'counsel-codesearch 'counsel-codesearch-occur)
+  )
+
 (use-package counsel
   :ensure t
   :config
+  (global-unset-key (kbd "C-x f"))
+  (bind-keys* :prefix-map counsel-search-map
+	      :prefix "C-x f")
+  (bind-keys :map counsel-search-map
+	     ("c" . counsel-codesearch)
+	     ("r" . counsel-rg)
+	     ("a" . counsel-projectile-ag))
   (setq gud-key-prefix "\C-]") ; doesn't matter what, just to remove C-x C-a, which is bound to ag
-  :bind(("C-x C-a" . 'counsel-rg) ;search in all files from current folder onwards
-	("C-x C-b" . 'counsel-projectile-ag) ;search only in project files
-	)
+  :bind(("C-x C-a" . 'counsel-codesearch-or-rg)) ;use codesearch if possible, fallback to rg
+  :requires
+  (counsel-codesearch)
   )
 
 
